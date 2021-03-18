a703eecf62bc943b6b73eb277413c8acadb50f00
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-18 15:49:10 -0600

    Create longest_line_length!/1 function

--- files.ex
+++ files.ex
@@ -1,13 +1,26 @@
 defmodule MyFile do
-  def large_lines!(path) do
-    File.stream!(path)
+  defp filtered_lines(path) do
+    path
+    |> File.stream!
     |> Stream.map(&String.replace(&1, "\n", ""))
+  end
+
+  def large_lines!(path) do
+    path
+    |> filtered_lines
     |> Enum.filter(&(String.length(&1) > 80))
   end
 
   def line_lengths!(path) do
-    File.stream!(path)
+    path
     |> Stream.map(&String.replace(&1, "\n", ""))
     |> Enum.map(&String.length/1)
+  end
+
+  def longest_line_length!(path) do
+    path
+    |> filtered_lines
+    |> Stream.map(&String.length/1)
+    |> Enum.max
   end
 end
