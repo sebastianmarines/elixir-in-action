60e65cd32a33dd330059d85d623be6f256dc6335
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-18 08:26:02 -0600

    Add a tail recursive implementation of the range function

--- range.ex
+++ range.ex
@@ -6,4 +6,16 @@ defmodule MyRange do
 	def range(n1, n2) do
 		[n1 | range(n1 + 1, n2)]
 	end
+
+	def range_tr(n1, n2) do
+		range_tr_h(n1, n2, [])
+	end
+
+	defp range_tr_h(n1, n2, acc) when n1 > n2 do
+		acc
+	end
+
+	defp range_tr_h(n1, n2, acc) do
+		range_tr_h(n1, n2 - 1, [n2 | acc])	
+	end
 end
